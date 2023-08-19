2f8ba355fd08a69c2d4e5be5c495b97eecd923a8
guilherme_urbanas <guilherme.urbanas@ocumetra.com>
2023-08-19 20:48:37 +0100

    Implement game room system

--- source/socketio/events/game_room.py
+++ source/socketio/events/game_room.py
@@ -1,9 +1,9 @@
 from flask import request
-from flask_socketio import Namespace, join_room, send, emit
+from flask_socketio import Namespace, join_room
 from uuid import uuid4
-import logging
 
 from source.models.game_room.game_room import GameRoom
+
 
 class GameRoomSocket(Namespace):
     def on_connect(self):
@@ -13,27 +13,31 @@ class GameRoomSocket(Namespace):
         print(f"Client disconnected from {self.namespace}: {request.sid}")
 
     def on_create_game_room(self, data):
+        print(f"[{self.namespace}]: {request.sid} joined the room [{data['name']}]")
         game_room = GameRoom(
-            uuid4(), 
-            data['player_id'], 
-            data['name'], 
-            data['max_players'], 
-            data['hearts'], 
-            data['turn_time_seconds'], 
-            data.get("password")
+            str(uuid4()),
+            data["player_id"],
+            data["name"],
+            data["max_players"],
+            data["hearts"],
+            data["turn_time_seconds"],
+            data.get("password"),
         )
-        
+
         join_room(game_room.id)
 
         data = {
-            "id": str(game_room.id),
+            "id": game_room.id,
             "player_id": game_room.player_id,
             "name": game_room.name,
             "password": game_room.password,
             "max_players": game_room.max_players,
             "hearts": game_room.hearts,
-            "turn_time_seconds": game_room.turn_time_seconds
+            "turn_time_seconds": game_room.turn_time_seconds,
         }
-        
-        emit('create_game_room', data, to=game_room.id)
 
+        self.emit("create_game_room", data, room=game_room.id)
+
+    def on_leave_game_room(self, data):
+        print(f"[{self.namespace}]: {request.sid} left the room {data['id']}")
+        self.emit("leave_game_room", data, room=data["id"])
--- source/socketio/instance.py
+++ source/socketio/instance.py
@@ -1,7 +1,6 @@
 from flask_socketio import SocketIO
+
 from source.socketio.events.game_room import GameRoomSocket
-from source.constants.socketio import GAME_ROOM_NAMESPACE
 
 socketio: SocketIO = SocketIO(cors_allowed_origins="*", logger=True)
 socketio.on_namespace(GameRoomSocket("/game-room"))
-import source.socketio.events.index
