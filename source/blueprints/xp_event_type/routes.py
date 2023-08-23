e20e05ca5f683e6f62ec9af8f3ebbb7699451f69
Leo-MouraF <leom.frigotto@gmail.com>
2023-08-23 16:14:23 -0300

    Revert "Show the xp_event_types data resource."
    This reverts commit ae92ef2497fd4a21812e079e8d63feb08ec737e8.

--- source/blueprints/xp_event_type/routes.py
+++ source/blueprints/xp_event_type/routes.py
@@ -2,9 +2,6 @@
 from flask import Blueprint, jsonify
 
 from source.constants.blueprints import XP_EVENT_TYPE_BLUEPRINT_NAME
-from source.models.xp_event_type.xp_event_type import XpEventType
-from source.dtos.xp_event_type import XpEventTypeResourceDTO
-from source.lib.responses import DataResponse
 
 xp_event_type_bp = Blueprint(
     XP_EVENT_TYPE_BLUEPRINT_NAME,
@@ -17,19 +14,3 @@ xp_event_type_bp = Blueprint(
 def xp_event_type_index():
     """Index request."""
     return jsonify({"data": "XP Event Type"})
-
-    
-@xp_event_type_bp.route("/xp_events")
-def read_xp_event():
-    """Get all the XP event types."""
-
-    events = XpEventType.query.all()
-    data = XpEventTypeResourceDTO(many=True).dump(events)
-    
-    return DataResponse(
-        "XP event types found successfully",
-        200,
-        {
-           "xp_event_types" : data
-        },
-    ).json()
