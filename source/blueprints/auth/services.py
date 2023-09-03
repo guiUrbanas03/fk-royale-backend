f1d3427f4dc60021cbd914ceadbd076961131329
guilherme_urbanas <guilherme.urbanas@ocumetra.com>
2023-09-03 12:07:04 +0100

    feature/FKR-3: Get user from token

--- source/blueprints/auth/services.py
+++ source/blueprints/auth/services.py
@@ -1,9 +1,12 @@
+from flask_jwt_extended import decode_token
 from typing import Union
 from uuid import uuid4
 
+from source.blueprints.user.services import get_user_by_id
 from source.database.instance import db
 from source.dtos.revoked_token import CreateRevokedTokenDTO
 from source.models.revoked_token.revoked_token import RevokedToken
+from source.models.user.user import User
 
 
 def create_revoked_token(token_data: CreateRevokedTokenDTO) -> RevokedToken:
@@ -42,3 +45,18 @@ def get_revoked_token_by_token(token: str) -> Union[RevokedToken, None]:
     (RevokedToken|None)
     """
     return RevokedToken.query.filter_by(token=token).one_or_none()
+
+
+def get_user_by_token(token: str) -> User:
+    """Get user from access token.
+
+    Parameters
+    ----------
+    token: str
+
+    Returns
+    -------
+    user: User
+    """
+    decoded_token: dict = decode_token(token)
+    return get_user_by_id(decoded_token["sub"])
