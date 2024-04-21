15fe3d995a3725de2175cf8bb076d857e93cdbf9
guilherme_urbanas <guilherme.urbanas@ocumetra.com>
2024-04-21 19:31:56 +0100

    Define access token expiration

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,7 @@
+import datetime
+
 """Global app settings."""
 FLASK_DEBUG = True
 FLASK_ENV = "development"
+FLASK_JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=10)
+JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=10)
